6d593cfcd171b00e58f084b1815219ae80c2ca1b
nick bui <tronggiao1908@gmail.com>
2025-07-17 17:18:49 -0700

    fix: update /about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
           As a rising Senior at Vanderbilt University, my academic path in Computer Science & Mathematics is enriched by active roles as an Applied Math Tutor, where I harness my Math, leadership, and communication skills to deepen students' understanding of complex concepts as well as foster collaboration and inclusion within our diverse community.
         </p>
         <p>
-          My time at Karpool solidified my commitment to using my skills in Mathematics and Computer Science to support organizations in making data-informed decisions. With every challenge, my passion for problem-solving and creating efficient, impactful solutions grows, guided by my dedication to continuous learning and improvement.
+          My time at TransAstra solidified my commitment to using my skills in Mathematics and Computer Science to build robust, scalable systems that solve real-world problems. With every challenge, my passion for problem-solving and creating efficient, impactful solutions grows, guided by my dedication to continuous learning and improvement.
         </p>
       </div>
     </section>
