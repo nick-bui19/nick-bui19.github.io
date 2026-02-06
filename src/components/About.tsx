a887c9a9a4f1fe4973d4a474a7624cf348a652b1
nick bui <tronggiao1908@gmail.com>
2026-02-06 09:46:36 -0600

    fix: update senior status

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
       <div className="about-content">
         <h2>My Background / About Me</h2>
         <p>
-          As a rising Senior at Vanderbilt University, my academic path in Computer Science & Mathematics is enriched by active roles as an Applied Math Tutor, where I harness my Math, leadership, and communication skills to deepen students' understanding of complex concepts as well as foster collaboration and inclusion within our diverse community.
+          As a Senior at Vanderbilt University, my academic path in Computer Science & Mathematics is enriched by active roles as an Applied Math Tutor, where I harness my Math, leadership, and communication skills to deepen students' understanding of complex concepts as well as foster collaboration and inclusion within our diverse community.
         </p>
         <p>
           My time at TransAstra solidified my commitment to using my skills in Mathematics and Computer Science to build robust, scalable systems that solve real-world problems. With every challenge, my passion for problem-solving and creating efficient, impactful solutions grows, guided by my dedication to continuous learning and improvement.
