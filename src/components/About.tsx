b88a26c2c8350925f266f49d837708f3016f7166
Nick Bui <125774779+nick-bui19@users.noreply.github.com>
2026-08-30 14:22:57 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
       <div className="about-content">
         <h2>My Background / About Me</h2>
         <p>
-          As a Senior at Vanderbilt University, my academic path in Computer Science & Mathematics is enriched by active roles as an Applied Math Tutor, where I harness my Math, leadership, and communication skills to deepen students' understanding of complex concepts as well as foster collaboration and inclusion within our diverse community.
+          Computer Science & Mathematics double major from Vanderbilt University with experience in software engineering, distributed systems, and cloud technologies. Skilled in C++, Java, Python, AWS, Kafka, Spark, Node.js, and TypeScript.
         </p>
         <p>
           At Future Standard, I support technology solutions for the private markets industry. I work across technical and business functions to help clients improve data workflows, solve operational challenges, and unlock value through technology.
